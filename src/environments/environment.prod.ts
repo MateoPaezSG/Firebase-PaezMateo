a4dc21db2b612a2d77d46f1665094becef66ec30
unknown <paez94144@gmail.com>
2023-04-17 11:38:36 -0500

    Aplicacion conectada con Firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAT_aBIvthDYpJofmI5otALO6uaHl9YL4E",
+    authDomain: "insumo1mateopaezg2.firebaseapp.com",
+    projectId: "insumo1mateopaezg2",
+    storageBucket: "insumo1mateopaezg2.appspot.com",
+    messagingSenderId: "804052437616",
+    appId: "1:804052437616:web:ae6a96b606116bbb3702b5"
+  }
+};
